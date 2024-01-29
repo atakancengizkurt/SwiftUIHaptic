103ecdf1be398f6197c783f2bcbd595b317238c3
Atakan Cengiz KURT <atakan1903@hotmail.com>
2024-01-29 11:16:11 +0300

    Initial Commit

---
+++ Haptic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Haptic
+//
+//  Created by Atakan Cengiz KURT on 29.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Haptic/HapticApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticApp.swift
+//  Haptic
+//
+//  Created by Atakan Cengiz KURT on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HapticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
