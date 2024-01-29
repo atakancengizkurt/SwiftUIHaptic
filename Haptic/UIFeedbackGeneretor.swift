5f7aee8e6e0313409c2bcdb2921704d3c038488c
Atakan Cengiz KURT <atakan1903@hotmail.com>
2024-01-29 13:29:12 +0300

    first commit

---
+++ Haptic/UIFeedbackGeneretor.swift
@@ -0,0 +1,18 @@
+//
+//  UIFeedbackGeneretor.swift
+//  Haptic
+//
+//  Created by Atakan Cengiz KURT on 29.01.2024.
+//
+
+import SwiftUI
+
+struct UIFeedbackGeneretor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIFeedbackGeneretor()
+}
